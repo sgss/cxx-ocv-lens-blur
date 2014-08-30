840a03ee1c3ee1b684b972dd8a505d6f0ed73587
Shota Matsuda <s@sgss.co>
2014-08-30 17:17:25 +0900

    removed cpplint's filter excludes

--- scripts/cpplint.sh
+++ scripts/cpplint.sh
@@ -3,20 +3,11 @@
 readonly SRCROOT=$(cd "$(dirname "$0")/../"; pwd)
 
 for FILE in $(find "$SRCROOT/src" -type f -name "*.cc" -o -name "*.c"); do
-  python "$SRCROOT/cpplint/cpplint.py" \
-      --root src \
-      --filter=-build/include_order,-readability/braces \
-      "$FILE"
+  python "$SRCROOT/cpplint/cpplint.py" --root src "$FILE"
 done
 for FILE in $(find "$SRCROOT/include" -type f -name "*.h"); do
-  python "$SRCROOT/cpplint/cpplint.py" \
-      --root include \
-      --filter=-build/include_order,-readability/braces \
-      "$FILE"
+  python "$SRCROOT/cpplint/cpplint.py" --root include "$FILE"
 done
 for FILE in $(find "$SRCROOT/tests" -type f -name "*.cc"); do
-  python "$SRCROOT/cpplint/cpplint.py" \
-      --root tests \
-      --filter=-build/include_order,-readability/braces \
-      "$FILE"
+  python "$SRCROOT/cpplint/cpplint.py" --root tests "$FILE"
 done
